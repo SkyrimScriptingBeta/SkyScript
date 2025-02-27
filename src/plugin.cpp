c28e5be1e1b23c165b38dc3d56da62865e680a56
Mrowr Purr <mrowr.purr@gmail.com>
2025-02-26 20:30:59 -0800

    Can enter any JS and run it

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -5,31 +5,13 @@
 
 std::unique_ptr<Nodejs::NodejsEnvironment> nodejs_environment = nullptr;
 
-inline void
-CallMeFromPapyrus(RE::BSScript::Internal::VirtualMachine*, RE::VMStackID, RE::StaticFunctionTag*) {
-    RE::ConsoleLog::GetSingleton()->Print("Hello, from C++ that Papyrus called.");
-}
-
-inline void
-MakeNodeJsEnvironment(RE::BSScript::Internal::VirtualMachine*, RE::VMStackID, RE::StaticFunctionTag*) {
-    RE::ConsoleLog::GetSingleton()->Print("Creating Node.js environment...");
-    if (nodejs_environment) {
-        RE::ConsoleLog::GetSingleton()->Print("Node.js environment already exists.");
-        return;
-    }
-
-    nodejs_environment = std::make_unique<Nodejs::NodejsEnvironment>();
-
-    RE::ConsoleLog::GetSingleton()->Print("Node.js environment created.");
-}
-
 inline void EvalJs(
     RE::BSScript::Internal::VirtualMachine* a_vm, RE::VMStackID a_stackID, RE::StaticFunctionTag*,
     std::string_view a_code
 ) {
     if (!nodejs_environment) {
-        RE::ConsoleLog::GetSingleton()->Print("Node.js environment not created.");
-        return;
+        RE::ConsoleLog::GetSingleton()->Print("Node.js environment not created, creating one...");
+        nodejs_environment = std::make_unique<Nodejs::NodejsEnvironment>();
     }
 
     RE::ConsoleLog::GetSingleton()->Print("Evaluating JavaScript code...");
@@ -54,8 +36,6 @@ extern "C" __declspec(dllexport) bool SKSEPlugin_Load(const SKSE::LoadInterface*
     });
 
     SKSE::GetPapyrusInterface()->Register([](RE::BSScript::IVirtualMachine* vm) {
-        vm->RegisterFunction("CallMeFromPapyrus", "SomeKeyboardBinds", CallMeFromPapyrus);
-        vm->RegisterFunction("MakeNodeJsEnvironment", "SomeKeyboardBinds", MakeNodeJsEnvironment);
         vm->RegisterFunction("EvalJs", "SomeKeyboardBinds", EvalJs);
         return true;
     });
